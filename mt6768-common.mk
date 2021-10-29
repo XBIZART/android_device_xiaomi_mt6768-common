6b3e9f07a453b78e558d14335cfb7cf0c399588c
fourninesix <klmajor2213@gmail.com>
2021-10-29 08:37:28 +0000

    mt6768-common: Build few wifi blobs from source

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -345,6 +345,27 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+# WifiConfig
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0 \
+    android.hardware.wifi@1.0.vendor \
+    android.hardware.wifi@1.1 \
+    android.hardware.wifi@1.1.vendor \
+    android.hardware.wifi@1.2 \
+    android.hardware.wifi@1.2.vendor \
+    android.hardware.wifi@1.3 \
+    android.hardware.wifi@1.3.vendor \
+    android.hardware.wifi.supplicant@1.0 \
+    android.hardware.wifi.supplicant@1.0.vendor \
+    android.hardware.wifi.supplicant@1.1 \
+    android.hardware.wifi.supplicant@1.1.vendor \
+    android.hardware.wifi.supplicant@1.2 \
+    android.hardware.wifi.supplicant@1.2.vendor \
+    android.hardware.wifi.hostapd@1.0 \
+    android.hardware.wifi.hostapd@1.0.vendor \
+    android.hardware.wifi.hostapd@1.1 \
+    android.hardware.wifi.hostapd@1.1.vendor
+
 PRODUCT_PACKAGES += \
     WifiOverlay \
     TetheringConfigOverlay
