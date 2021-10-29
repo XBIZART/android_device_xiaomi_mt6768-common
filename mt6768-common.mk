f84e80597f389394e54cd2a61234279af2c070f8
fourninesix <klmajor2213@gmail.com>
2021-10-29 09:32:01 +0000

    mt6768-common: Build sensorservice 1.0 HAL

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -283,6 +283,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
     android.hardware.sensors@2.0 \
     android.hardware.sensors@2.0.vendor \
     android.hardware.sensors@2.0-impl \
