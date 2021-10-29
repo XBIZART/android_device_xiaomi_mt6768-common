6f5846b7065b4068b2d5708141897e06798743d2
fourninesix <klmajor2213@gmail.com>
2021-10-29 08:52:17 +0000

    mt6768-common: Build few radio config and vendor variants from source

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -264,7 +264,13 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
     android.hardware.radio@1.4.vendor \
     android.hardware.radio@1.5 \
-    android.hardware.radio@1.5.vendor
+    android.hardware.radio@1.5.vendor\
+    android.hardware.radio.config@1.0 \
+    android.hardware.radio.config@1.0.vendor \
+    android.hardware.radio.config@1.1 \
+    android.hardware.radio.config@1.1.vendor \
+    android.hardware.radio.config@1.2 \
+    android.hardware.radio.config@1.2.vendor 
 
 # Seccomp
 PRODUCT_COPY_FILES += \
