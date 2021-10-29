30d495984ffe8c96a06b192fda8e64d14d46b076
fourninesix <klmajor2213@gmail.com>
2021-10-29 09:13:24 +0000

    mt6768-common: Build android.hardware.thermal@2.0 from source
    Signed-off-by: fourninesix <klmajor2213@gmail.com>

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -297,7 +297,9 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@2.0-impl
+    android.hardware.thermal@2.0-impl \
+    android.hardware.thermal@2.0 \
+    android.hardware.thermal@2.0.vendor
 
 # TinyXML
 PRODUCT_PACKAGES += \
